804df592da34a808a799ab0240b7e9fc0cfdc6e0
Frank Delporte <fdelporte@azul.com>
2022-12-14 17:04:02 +0100

    Correct install and log directory

--- service/start_pi4j_board_info_service.sh
+++ service/start_pi4j_board_info_service.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 JAVA_HOME=/home/ubuntu/.sdkman/candidates/java/current
-WORKDIR=/home/ubuntu/raspberry-pi-info-service/
+WORKDIR=/home/ubuntu/pi4j-board-info-service/
 #JAVA_OPTIONS="-Xms256m -Xmx512m -server"
 JAVA_OPTIONS=""
 #APP_OPTIONS="-c /path/to/app.config -d /path/to/datadir"
 APP_OPTIONS=""
 
 cd $WORKDIR
-"${JAVA_HOME}/bin/java" $JAVA_OPTIONS -jar raspberry-pi-info-service.jar $APP_OPTIONS &
+"${JAVA_HOME}/bin/java" $JAVA_OPTIONS -jar pi4j-board-info-service.jar $APP_OPTIONS &
